648ee5f87145d2a021950168ce9aeb597852a97e
Chovatiyatanvi <tanvichovatiya0337@gmail.com>
2024-08-22 20:33:04 +0530

    Add files via upload

---
+++ task1/script6.php
@@ -0,0 +1,10 @@
+<?php
+echo"<b>tanvi chovatiya</b><br>";
+echo"<i>tanvi chovatiya</i><br>";
+echo"<h1>tanvi chovatiya</h1><br>";
+echo"<h2>tanvi chovatiya</h2><br>";
+echo"<h3>tanvi chovatiya</h3><br>";
+echo"<h4>tanvi chovatiya</h4><br>";
+echo"<h5>tanvi chovatiya</h5><br>";
+echo"<h6>tanvi chovatiya</h6><br>";
+?>
