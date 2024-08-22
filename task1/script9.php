3c427d489a5f3023ff8f896285bbfbd96362f734
Chovatiyatanvi <tanvichovatiya0337@gmail.com>
2024-08-22 20:34:44 +0530

    Add files via upload

---
+++ task1/script9.php
@@ -0,0 +1,10 @@
+<?php
+$name="tanvi";
+$age=20;
+$city="rajkot";
+$mobile=1234567897;
+echo"my name is  ".$name."<br>";
+echo"my age is  ".$age."<br>";
+echo"my city is  ".$city."<br>";
+echo"my mobile no is  ".$mobile;
+?>
